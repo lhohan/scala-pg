e9c3acb7ed2af38be68e3e32476d9a0d1d2fc043
lhohan <github@lhoest.eu>
2013-08-27 18:09:45 +0200

    Implemented Listener actor

--- akka/src/main/scala/pi/Pi.scala
+++ akka/src/main/scala/pi/Pi.scala
@@ -40,4 +40,14 @@ object Pi extends App {
     }
   }
 
+  class Listener extends Actor {
+    def receive = {
+      case PiApproximation(pi, duration) => {
+        println("\n\tPi approximation: \t\t%s\n\tCalculation time: \t%s"
+          .format(pi, duration))
+        context.system.shutdown()
+      }
+    }
+  }
+
 }
