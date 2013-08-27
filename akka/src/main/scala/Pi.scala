364de53255ba85c34228f07baf51e7aa166ab21f
lhohan <github@lhoest.eu>
2013-08-27 17:54:07 +0200

    Start implementing Pi as described on (using akka 2.2) http://doc.akka.io/docs/akka/2.0/intro/getting-started-first-scala.html
    Empty class

---
+++ akka/src/main/scala/Pi.scala
@@ -0,0 +1,15 @@
+/**
+ * User: hanlho
+ * DateTime: 27/08/13 17:45
+ *
+ * http://doc.akka.io/docs/akka/2.0/intro/getting-started-first-scala.html
+ */
+
+import akka.actor._
+import akka.routing.RoundRobinRouter
+import scala.concurrent.duration.Duration
+import scala.concurrent.duration._
+
+class Pi {
+
+}
