023bff9c0c71e22098bf2bc12e56a4c1d475e52a
lhohan <github@lhoest.eu>
2013-08-27 18:04:36 +0200

    Implemented Worker actor

--- akka/src/main/scala/pi/Pi.scala
+++ akka/src/main/scala/pi/Pi.scala
@@ -24,4 +24,19 @@ class Pi {
 
   case class PiApproximation(pi: Double, duration: Duration) extends PiMessage
 
+  class Worker extends Actor {
+
+    def calculatePiFor(start: Int, nrOfElements: Int): Double = {
+      var acc = 0.0
+      for (i ← start until (start + nrOfElements))
+        acc += 4.0 * (1 - (i % 2) * 2) / (2 * i + 1)
+      acc
+    }
+
+    def receive = {
+      case Work(start, nrOfElements) => sender ! Result(calculatePiFor(start, nrOfElements))
+
+    }
+  }
+
 }
