1b403cdd6f8743bfa5b95cc76e5a9ca81c5a5785
lhohan <github@lhoest.eu>
2015-01-25 17:52:04 +0100

    Sudoko Solver - Scala version of solution listed in Pearls of Functional Programming by Richard Bird

---
+++ src/main/scala/misc/SudokuSolver.scala
@@ -0,0 +1,92 @@
+package misc
+
+/**
+ * Created by hans on 25/01/15.
+ */
+object SudokuSolver {
+
+  // TODO : only stream in case of infinite lists ...?
+  // TODO: consistent method signature: functional args as it is more in spirit of book and functional style
+
+  type Row[A] = Stream[A]
+  type Matrix[A] = Stream[Row[A]]
+  type Digit = Int
+  // Book: Char, keep it more simple, less typing during tests
+  type Grid = Matrix[Digit]
+  val digits = (1 to 9).toStream
+
+  val Blank = 0
+  def blank = (x: Int) => x == Blank
+
+  type Choices = Stream[Digit]
+
+  // given a grid, create matrix where each field contains a Stream of possible choices
+  def choices(g: Grid): Matrix[Choices] = {
+    def choice(d: Digit): Choices = if (blank(d)) digits else Stream(d)
+    g.map(_.map(choice)) // TODO: can be more concise?
+  }
+
+  // given the result of `choices` expand it to a Stream of Grids with all possible combinations
+  def expand(mc: Matrix[Choices]): Stream[Grid] = {
+    val possibleChoicesForEachRow = mc.map(cartesianProduct)
+    cartesianProduct(possibleChoicesForEachRow) // think about it, write it down to understand
+  }
+
+  def cartesianProduct[A](m: Stream[Stream[A]]): Stream[Stream[A]] = m match {
+    case Stream() => Stream(Stream.empty[A])
+    case xs #:: xss => for {
+      x <- xs
+      ys <- cartesianProduct(xss)
+    } yield x #:: ys
+  }
+
+
+  // filter to find out the valid grids = no duplicates in rows, cols and boxes
+  def valid(g: Grid): Boolean =
+    cols(g).forall(noDups) &&
+    rows(g).forall(noDups) &&
+    boxs(g).forall(noDups)
+
+  def noDups[A]: Stream[A] => Boolean = {
+    case Stream() => true
+    case x #::xs => xs.forall(x != _) && noDups(xs)
+  }
+
+  def rows[A]: (Matrix[A]) => Matrix[A] = m => m
+
+  def cols[A]: (Matrix[A]) => Matrix[A] = {
+    // matrix transposition
+    case Stream(xs) => xs.map(Stream(_))
+    case xs #:: xss => xs.zip(cols(xss)).map{case (x,y) => x #:: y}
+  }
+
+  def printable[A](m:Matrix[A]):List[List[A]] = m.map(_.toList).toList
+
+  def boxs[A]: (Matrix[A]) => Matrix[A] = m => {
+    // draw the sequence to understand
+
+    // we 'collapse' (group) the parts in the matrix to the units we want to move around
+    val grouped = group(m.map(group))
+    // then we move to get our parts in their place, transpose each grouped part
+    val transposed = grouped.map(cols)
+    // everything is place so: flatten everything again
+    val ungrouped = ungroup(transposed).map(ungroup)
+    ungrouped
+  }
+
+  // split list in groups of 3
+  def group[A]: Stream[A] => Stream[Stream[A]] =  {
+    case Stream() => Stream.empty[Stream[A]]
+    case xs => xs.take(3) #:: group(xs.drop(3))
+  }
+
+  def ungroup[A]: Stream[Stream[A]] => Stream[A] = xss => for {
+    xs <- xss
+    x <- xs
+  } yield x
+
+  // nice expression of how we are solving
+  def solve(g: Grid): Stream[Grid] = expand(choices(g)) filter valid
+
+
+}
---
+++ src/test/scala/misc/SudokuSolverTest.scala
@@ -0,0 +1,153 @@
+package misc
+
+import org.scalatest.FunSuite
+
+import scala.concurrent.{Await, Future}
+
+/**
+ * Created by hans on 25/01/15.
+ */
+class SudokuSolverTest extends FunSuite {
+
+import misc.SudokuSolver._
+
+  test("some stuff") {
+    val m: Matrix[Int] = Stream(Stream(1, 2, 3), Stream(4, 5, 6))
+    val g: Grid = Stream(Stream(1, 2, 3))
+  }
+
+  test("cartesian product") {
+    val arg = Stream(Stream(1, 2), Stream(3), Stream(4, 5))
+    val result = cartesianProduct(arg)
+    assertResult(Stream(Stream(1, 3, 4), Stream(1, 3, 5), Stream(2, 3, 4), Stream(2, 3, 5))) {
+      //      println(s"    ${cartesianProduct(result).map(_.toList).toList}")
+      result
+    }
+  }
+
+  test("rows") {
+    assertResult(Stream(1, 7, 2, 5, 4, 9, 6, 8, 3)) {
+      rows(solved).head
+    }
+  }
+
+  test("cols") {
+    assertResult(Stream(1, 6, 3, 4, 8, 2, 9, 7, 5)) {
+      cols(solved).head
+    }
+  }
+
+  test("boxes") {
+    assertResult(Stream(1, 7, 2, 6, 4, 5, 3, 8, 9)) {
+      boxs(solved).head
+    }
+  }
+
+  test("ungroup") {
+    assertResult(Stream(1, 7, 2, 6, 4, 5, 3, 8, 9)) {
+      ungroup(Stream(Stream(1, 7, 2), Stream(6, 4, 5), Stream(3, 8, 9)))
+    }
+  }
+
+  test("group") {
+    assertResult(Stream(Stream(1, 7, 2), Stream(6, 4, 5), Stream(3, 8, 9))) {
+      group(Stream(1, 7, 2, 6, 4, 5, 3, 8, 9))
+    }
+  }
+
+  test("nodups"){
+    assertResult(true){
+      noDups(Stream(1,4,5,6,7))
+    }
+    assertResult(true){
+      noDups(Stream())
+    }
+    assertResult(true){
+      noDups(Stream(1))
+    }
+    assertResult(false){
+      noDups(Stream(2,3,5,2))
+    }
+  }
+
+  test("solve - version one of solver, trivial case") {
+    assertResult(solved) {
+      solve(Stream(
+        Stream(1, 7, 2, 5, 4, 9, 6, 8, 3),
+        Stream(6, 4, 5, 8, 7, 3, 2, 1, 9),
+        Stream(3, 8, 9, 2, 6, 1, 7, 4, 5),
+        Stream(4, 9, 6, 3, 2, 7, 8, 5, 1),
+        Stream(8, 1, 3, 4, 5, 6, 9, 7, 2),
+        Stream(2, 5, 7, 1, 9, 8, 4, 3, 6),
+        Stream(9, 6, 4, 7, 1, 5, 3, 2, 8),
+        Stream(7, 3, 1, 6, 8, 2, 5, 9, 4),
+        Stream(5, 2, 8, 9, 3, 4, 1, 6, 7)
+      )).toList.head
+    }
+  }
+
+  val X = Blank
+
+  def printTime(value: Long, s: String) = println(s"== Time $value ms for $s")
+
+  test("solve - version one of solver, one blanked") {
+    assertResult(solved) {
+      val (elapsed, result) = time(solve(Stream(
+        Stream(X, 7, 2, 5, 4, 9, 6, 8, 3),
+        Stream(6, 4, 5, 8, 7, 3, 2, 1, 9),
+        Stream(3, 8, 9, 2, 6, 1, 7, 4, 5),
+        Stream(4, 9, 6, 3, 2, 7, 8, 5, 1),
+        Stream(8, 1, 3, 4, 5, 6, 9, 7, 2),
+        Stream(2, 5, 7, 1, 9, 8, 4, 3, 6),
+        Stream(9, 6, 4, 7, 1, 5, 3, 2, 8),
+        Stream(7, 3, 1, 6, 8, 2, 5, 9, 4),
+        Stream(5, 2, 8, 9, 3, 4, 1, 6, 7)
+      )).toList)
+      printTime(elapsed, "version one of solver, one blanked")
+      result.head
+    }
+  }
+
+  import scala.concurrent.duration._
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  def time[A](x: => A):(Long, A) = {
+    val start = System.currentTimeMillis()
+    val result = Await.result(Future(x), 10 seconds)
+    val end = System.currentTimeMillis()
+    (end - start, result)
+  }
+
+  test("solve - version one of solver, original puzzle") {
+    assertResult(solved) {
+      val (elapsed, result) = time(solve(Stream(
+        Stream(X, X, X, X, X, X, 6, 8, X),
+        Stream(X, X, X, X, 7, 3, X, X, 9),
+        Stream(3, X, 9, X, X, X, X, 4, 5),
+        Stream(4, 9, X, X, X, X, X, X, X),
+        Stream(8, X, 3, X, 5, X, 9, X, 2),
+        Stream(X, X, X, X, X, X, X, 3, 6),
+        Stream(9, 6, X, X, X, X, 3, X, 8),
+        Stream(7, X, X, 6, 8, X, X, X, X),
+        Stream(X, 2, 8, X, X, X, X, X, 7)
+      )).toList)
+      printTime(elapsed, "version one of solver, original puzzle")
+      result.head
+    }
+  }
+
+  def mkString: Grid => String = g => "\n"+g.map(_.mkString("|")).mkString("\n")
+
+  lazy val solved: Grid = Stream(
+    Stream(1, 7, 2, 5, 4, 9, 6, 8, 3),
+    Stream(6, 4, 5, 8, 7, 3, 2, 1, 9),
+    Stream(3, 8, 9, 2, 6, 1, 7, 4, 5),
+    Stream(4, 9, 6, 3, 2, 7, 8, 5, 1),
+    Stream(8, 1, 3, 4, 5, 6, 9, 7, 2),
+    Stream(2, 5, 7, 1, 9, 8, 4, 3, 6),
+    Stream(9, 6, 4, 7, 1, 5, 3, 2, 8),
+    Stream(7, 3, 1, 6, 8, 2, 5, 9, 4),
+    Stream(5, 2, 8, 9, 3, 4, 1, 6, 7)
+  )
+
+}
